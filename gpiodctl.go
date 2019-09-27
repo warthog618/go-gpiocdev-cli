afeb519754f1d7606d838b28d1303fad1ff264c3
Kent Gibson <warthog618@gmail.com>
2019-09-27 19:18:43 +0800

    rename gpiod utility to gpiodctl

---
+++ gpiodctl.go
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright © 2019 Kent Gibson <warthog618@gmail.com>.
+
+// +build linux
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gpiodctl",
+	Short: "gpiodctl is a utility to control GPIO lines",
+	Long:  "gpiodctl is a utility to control GPIO lines on Linux GPIO character devices",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+	Version: version,
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func logErr(cmd *cobra.Command, err error) {
+	fmt.Fprintf(os.Stderr, "gpiodctl %s: %s\n", cmd.Name(), err)
+}
